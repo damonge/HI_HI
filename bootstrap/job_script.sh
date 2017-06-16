e62a34124bf39e6db1058ea09f0b79a16146925b
Andrej Obuljen <aobuljen@sissa.it>
2017-06-16 20:40:40 +0200

    code we currently use

--- bootstrap/job_script.sh
+++ bootstrap/job_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -l nodes=1:ppn=20
-#PBS -l walltime=02:30:00
+#PBS -l walltime=00:30:00
 #PBS -joe
 #PBS -mn
 #PBS -T flush_cache
@@ -17,4 +17,4 @@ module load scipy/0.15.1/numpy/1.9.1/intel/14.0/mkl/11.1/python/2.7.8
 export PYTHONPATH=$PYTHONPATH:$HOME/python/2.7.8/lib/python2.7/site-packages
 
 cd $PBS_O_WORKDIR
-python analysis_2D_bootstrap.py
+python analysis_3D_bootstrap_proper_fkp.py
